424e7869f07378bebb9a294cf2d0cea565bd7122
123hotdog1100 <123hotdog1100@gmail.com>
2021-07-26 00:25:39 +0100

    Added delete method handling

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 api = Api(app)
 
 put_args = reqparse.RequestParser()
-put_args.add_argument("prefix", type=str, help="Prefix of bot")
+put_args.add_argument("prefix", type=str, help="Prefix of bot", required=True)
+put_args.add_argument("twitch", type=str, help="Twtich username")
 
 ids = {}
 
@@ -14,6 +15,10 @@ def abort_if_not(ID):
     if ID not in ids:
         abort("ID is not valid")
 
+
+def abort_if_exists(ID):
+    if ID in ids:
+        abort(409, message="ID already exists")
 
 class Backend(Resource):
     def get(self, ID):
@@ -24,9 +29,15 @@ class Backend(Resource):
         return {"data": "posted"}
 
     def put(self, ID):
+        abort_if_exists(ID)
         args = put_args.parse_args()
         ids[ID] = args
         return ids[ID], 201
+
+    def delete(self, ID):
+        abort_if_not(ID)
+        del ids[ID]
+        return '', 204
 
 
 api.add_resource(Backend, "/helloworld/<int:ID>")
